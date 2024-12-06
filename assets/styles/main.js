6c807862ee236aedb8fce6b31558e27ba8f7ede5
Esraa gamal <eg6237561@gmail.com>
2024-12-06 19:34:08 +0200

    Add files via upload

---
+++ assets/styles/main.js
@@ -0,0 +1,6 @@
+let btnmenu=document.getElementById("btnmenu")
+let navbar=document.querySelector(".navbar")
+btnmenu.onclick = function(){
+  btnmenu.classList.toggle("fa-times")
+navbar.classList.toggle("active")
+}
