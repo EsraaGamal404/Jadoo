db708293f431e932b864146e64402f6a333311a9
esraa gamal <eg6237561@gmail.com>
2025-01-01 20:08:20 +0200

    Add: resposive section

--- assets/styles/main.js
+++
@@ -1,6 +0,0 @@
-let btnmenu=document.getElementById("btnmenu")
-let navbar=document.querySelector(".navbar")
-btnmenu.onclick = function(){
-  btnmenu.classList.toggle("fa-times")
-navbar.classList.toggle("active")
-}
